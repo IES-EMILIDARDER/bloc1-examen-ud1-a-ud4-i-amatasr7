447b6a0be55e0d36d13e0fb2da86add3b06c464e
amr02iesemilidarder <amr02@iesemilidarder.com>
2024-11-20 17:36:11 +0100

    EXAMEN

--- EXAMEN/src/examen/Main.java
+++ EXAMEN/src/examen/Main.java
@@ -1,16 +1,9 @@
 package examen;
 
-/**
- *
- * @author winadmin
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
